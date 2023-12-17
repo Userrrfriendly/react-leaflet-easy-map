329ec02cc1960186f2355ad9e1ac49bbc471b099
Ben <tsigourof.ben@gmail.com>
2023-12-17 18:04:02 +0200

    adds font-roboto

---
+++ src/Components/Main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+const Main = () => {
+  return (
+    <Box>
+      <Typography>Hello 👋 World 🌍</Typography>
+      <Typography variant="h1">Roboto</Typography>
+      <Typography variant="h2">Roboto</Typography>
+      <Typography variant="h3">Roboto</Typography>
+      <Typography variant="h4">Roboto</Typography>
+      <Typography variant="h5">Roboto</Typography>
+      <Typography variant="h6">Roboto</Typography>
+      <Typography variant="body1">Roboto</Typography>
+      <Typography variant="body2">Roboto</Typography>
+    </Box>
+  );
+};
+
+export default Main;
